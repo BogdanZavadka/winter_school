f3c4412e14377efe41f3fd5b20ad75dcc7c73570
BogdanZavadka <92270163+BogdanZavadka@users.noreply.github.com>
2022-02-02 20:19:15 +0200

    changed variables, added some function

--- main.py
+++ main.py
@@ -2,59 +2,48 @@ import datetime
 from datetime import date
 from typing import List, Union
 
-'''class Fish:
 
-    def __init__(self) -> None:
-        self.name = "oseledets"
-        self.price_in_uah_per_kilo = 11.2
-        self.catch_date = datetime("21/01/2022")
-        self.origin = "Norway"
-        self.body_only = True
-        self.weight = 100'''
+class Fish:
+    age_in_months = 0
+    weight = 0
+
+
+class FishInfo:
+    name = ''
+    origin = ''
+    catch_date = ''
+    due_date = ''
+
+
+class FishBox:
+    fish_info = FishInfo()
+    weight = 0
+    package_date = ''
+    is_fresh = True
+    weight = 0
+    height = 0
+    width = 0
+    length = 0
 
 
 class FishShop:
-    fish_name = ''
-    weight = 0
-    price_in_uah_per_kilo = 0
-    catch_date = date(2022, 1, 1)
 
-    expiration_date = date(2022, 2, 1)
+    frozen_fish_boxes: dict[str: List[FishBox]]
+    fresh_fish: dict[str: List[Fish]]
 
-    def add_fish(self, fish_name: str, weight, price_in_uah_per_kilo: float, catch_date: datetime) -> None:
-        self.fish_name = fish_name
-        self.weight = weight
-        self.price_in_uah_per_kilo = price_in_uah_per_kilo
-        self.catch_date = catch_date
-
-    #def get_fish_names_sorted_by_price(self) -> List[Union[str, float]]:
-    #   pass
-
-    def sell_fish(self, fish_name: str, weight: float) -> float:
-        if fish_name == self.fish_name:
-            return self.price_in_uah_per_kilo * weight
-
-    def cast_out_old_fish(self) -> str:
-        list1 = List
-        if self.catch_date >= self.expiration_date:
-            return self.fish_name
-
-
-class Seller:
-    def sort_fish_on_counter(self, fish_name) -> None:
+    def add_fish(self, fish_box: FishBox) -> None:
         pass
 
-    def show_product_information(self, fish_name, price_in_uah_per_kilo, catch_date, origin) -> List:
+    def sell_fish(self, name: int, weight: float, is_fresh: bool) -> None:
         pass
 
-    def sell_product(self, fish_name, weight) -> float:
+    def get_fish_names_sorted_by_price(self) -> List[Union[str, bool, float]]:
         pass
 
-
-class Buyer:
-    def check_freshness_of_product(self, catch_date) -> bool:
+    def get_fresh_fish_names_sorted_by_price(self) -> List[Union[str, float]]:
         pass
 
-    def buy_fish(self, price_in_uah_per_kilo, weight) -> float:
+    def get_frozen_fish_names_sorted_by_price(self) -> List[Union[str, float]]:
         pass
+
 
